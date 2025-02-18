8ce75c29b312c5eec749e9674703ccb5b677cc21
ppabam <tom@diginori.com>
2025-02-18 17:50:42 +0900

    촬칵

--- Main.py
+++ Main.py
@@ -1,9 +1,3 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
-from streamlit_extras.jupyterlite import jupyterlite
 
-st.title("CMS for flag123")
-
-
-jupyterlite(1500, 1600)
+st.camera_input("촬칵")
---
+++ run.sh
@@ -0,0 +1,4 @@
+
+#!/bin/bash
+
+streamlit run Main.py
